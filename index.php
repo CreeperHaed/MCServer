b20f1d4d48ef05e87ab8c6ef32c660d6c54f0b74
CreeperHaed <donlukasstadel@gmail.com>
2023-10-31 11:19:15 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="de">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Document</title>
+</head>
+<body>
+
+    <div class="sidebar closed">
+        <ul>
+            
+        </ul>
+    </div>
+
+    <div class="dimmer none"></div>
+
+    <div class="main">
+
+        <div class="menu">
+            <ul>
+                <li></li>
+                <li></li>
+                <li></li>
+            </ul>
+        </div>
+
+        <div class="content">
+
+        </div>
+
+    </div>
+
+</body>
+</html>
