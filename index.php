5a06282c8624348c8e0169e6897c4bfcb2284663
CreeperHaed <donlukasstadel@gmail.com>
2023-11-12 10:16:06 +0100

    Updated Index

--- index.php
+++ index.php
@@ -1,36 +1,29 @@
 <!DOCTYPE html>
 <html lang="de">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style.css">
-    <title>Document</title>
-</head>
-<body>
-
-    <div class="sidebar closed">
-        <ul>
-            
-        </ul>
-    </div>
-
-    <div class="dimmer none"></div>
-
-    <div class="main">
-
-        <div class="menu">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="style.css">
+        <title>Document</title>
+    </head>
+    <body>
+        <div class="sidebar closed">
             <ul>
-                <li></li>
-                <li></li>
-                <li></li>
+
             </ul>
+        </div>  
+        <div class="dimmer none"></div>
+        <div class="main">
+            <div class="menu">
+                <ul>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                </ul>
+            </div>
+            <div class="content">
+                
+            </div>
         </div>
-
-        <div class="content">
-
-        </div>
-
-    </div>
-
-</body>
+    </body>
 </html>
